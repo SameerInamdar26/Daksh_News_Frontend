accb323e1621a8c319d82430fc8b2150ee5b9745
Sameer <saminamdar2310@gmail.com>
2026-02-21 14:03:32 +0530

    Deployment fixed-2

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,6 +1,15 @@
-<img 
-  src={item.imageUrl} 
-  className="card-img-top" 
-  alt={item.title} 
-  style={{ borderTopLeftRadius: '8px', borderTopRightRadius: '8px' }}
-/>
+// frontend/src/components/NewsList.js
+import React from 'react';
+
+function NewsList({ item }) {
+  return (
+    <img 
+      src={item.imageUrl} 
+      className="card-img-top" 
+      alt={item.title} 
+      style={{ borderTopLeftRadius: '8px', borderTopRightRadius: '8px' }}
+    />
+  );
+}
+
+export default NewsList;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,8 +4,13 @@ import NewsList from '../components/NewsList';
 
 function Home() {
   return (
-    <div>
-      <h2 className="mb-4">ताज्या बातम्या</h2>
+    <div className="container my-4">
+      <h2 
+        className="mb-4" 
+        style={{ fontFamily: "'Baloo 2', cursive", fontWeight: 'bold', color: '#212121' }}
+      >
+        ताज्या बातम्या
+      </h2>
       <NewsList />
     </div>
   );
